ae58745b5f7aa9972f3749ba9444bfb9db915713
carlaval655 <carla.val655@gmail.com>
2022-08-24 17:20:45 -0400

    Funcionalidad terminada

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_print
 
 import 'package:flutter/material.dart';
 
@@ -31,6 +31,66 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  String output = "";
+
+  String _output = ""; //Output temporal para cambiar de estado output
+  double num1=0.0;
+  double num2=0.0;
+  String operacion="";
+
+  buttonPressed(String buttonText){
+    if (buttonText=="Clear"){
+      _output="0";
+      num1=0.0;
+      num2=0.0;
+      operacion="";
+    } else if(buttonText=="+" || buttonText=="-" || buttonText=="X" || buttonText=="/"){//Significa que el usuario ya ingreso un numero e ingresara el siguiente
+      num1=double.parse(output);
+
+      operacion=buttonText;
+
+      _output = "0";
+    } else if (buttonText=="."){
+      if(_output.contains(".")){
+        print("El numero ya tiene decimales");
+        return;
+      } else{
+        _output=_output+buttonText;
+      }
+    } else if(buttonText=="="){
+      num2= double.parse(output);
+      if (operacion=="+"){
+        _output=(num1+num2).toString();
+      }
+      if (operacion=="-"){
+        _output=(num1-num2).toString();
+      }
+      if (operacion=="X"){
+        _output=(num1*num2).toString();
+      }
+      if (operacion=="/"){
+        _output=(num1/num2).toString();
+      }
+
+      num1 = 0.0;
+      num2 = 0.0;
+      operacion = "";
+
+    } else{
+      _output = _output + buttonText;
+    }
+
+    print(_output);
+
+    setState(() {
+      
+      output=double.parse(_output).toStringAsFixed(2);
+
+    });
+
+  }
+
   Widget buildButton(String buttonText) {
     return Expanded(
       child: OutlinedButton(
@@ -39,7 +99,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
           // shape: CircleBorder(),
         ), 
-        onPressed: () {  },
+        onPressed: () {  
+          buttonPressed(buttonText);
+        },
         child: Text(buttonText,
         style: TextStyle(
           fontSize: 20,
@@ -59,7 +121,17 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Container(
         child: Column(children: <Widget>[
-          const Text("0"),
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.symmetric(
+              vertical: 24,
+              horizontal: 12,
+            ),
+            child: Text(output,
+            style:TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ))),
           const Expanded(
             child: Divider(),
           ),
